fc96c4aeff734b3a33cc4c208b6e152d386f6e8c
vinayk02 <80611746+vinayk02@users.noreply.github.com>
2023-03-15 16:39:01 -0400

    improvements to word choice/ updated info

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,48 +1,51 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class About extends Component {
-  render() {
-    if (!this.props.data) return null;
+    render() {
+        if (!this.props.data) return null;
 
-    
-    const profilepic = "images/" + this.props.data.image;
-    
+        const profilepic = 'images/' + this.props.data.image;
 
-    return (
-      <section id="about">
-        <div className="row">
-          <div className="three columns">
-            <img className="profile-pic" src={profilepic} alt-text="photo"  />
-          </div>
-          <div className="nine columns main-col">
-            <h2>About Me</h2>
+        return (
+            <section id="about">
+                <div className="row">
+                    <div className="three columns">
+                        <img className="profile-pic" src={profilepic} alt-text="photo" />
+                    </div>
+                    <div className="nine columns main-col">
+                        <h2>About Me</h2>
 
-            <p>I’m interested in app development and in web development. 
-              My previous experiences include developing and releasing my own app called Priorityze on the Apple app store and on the Google Play Store. 
-              I have also made several websites such as the Alloy Solutions website, MasterDate, Noel's Barbershop and some mock dashboards. 
-              I had an internship at Whiting-Turner in the summer of 2022 which taught me about several programs 
-              that are used in construction and how to use them in order to be a good engineer. 
-              I have previously managed the social media and website presence for Taste of India.
-             
-              I’m a fast and enthusiastic learner and am fluent in spoken Punjabi and Hindi. </p>
-            <div className="row">
-              <div className="columns contact-details">
-                <h2>Contact Me</h2>
-                <p className="address">
-                  <span>Name: Vinay Kumar</span>
-                  <br />
-                  <span>Email: kumarv242471@gmail.com</span>
-                  <br />
-                  <span>Phone Number: 646-691-4303</span>
-                </p>
-              </div>
-            
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  }
+                        <p>
+                            I'm a passionate developer as well as an entrepreneur. I have experience with both app, web
+                            development and currently experimenting with machine learning. I created an app called
+                            Priorityze with a vision to help people, including myself, to be more organized with their
+                            tasks in ways that matter to them. The app includes a to-do list and a calendar with many
+                            unique features. Thanks to Priorityze, thousands of users have been able to become their
+                            most productive selves. In the fall of 2021, I created Alloy Solutions, a company that
+                            provides services such as branding, web development, and app development. During my
+                            internship at Whiting-Turner, I gained valuable experience in the field of engineering. I
+                            learned about the various programs used in construction and how to utilize it to become a
+                            good engineer. In addition to technical skills, I am a fast and enthusiastic learn, fluent
+                            in spoken Punjabi and Hindi, and have a strong work ethic. I'm excited to explore new
+                            technologies and continue to grow my skills as a developer.{' '}
+                        </p>
+                        <div className="row">
+                            <div className="columns contact-details">
+                                <h2>Contact Me</h2>
+                                <p className="address">
+                                    <span>Name: Vinay Kumar</span>
+                                    <br />
+                                    <span>Email: kumarv242471@gmail.com</span>
+                                    <br />
+                                    <span>Phone Number: 646-691-4303</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default About;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,76 +1,78 @@
-import React, { Component } from "react";
-import ParticlesBg from "particles-bg";
+import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg';
 
 class Header extends Component {
-  render() {
-    
+    render() {
+        return (
+            <header id="home">
+                <ParticlesBg num={200} color="#FFFFFF" type="cobweb" bg={true} />
 
+                <nav id="nav-wrap">
+                    <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+                        Show navigation
+                    </a>
+                    <a className="mobile-btn" href="#home" title="Hide navigation">
+                        Hide navigation
+                    </a>
 
-    return (
-      <header id="home">
-        <ParticlesBg   num={200} color="#FFFFFF"type="cobweb" bg={true} />
+                    <ul id="nav" className="nav">
+                        <li className="current">
+                            <a className="smoothscroll" href="#home">
+                                Home
+                            </a>
+                        </li>
 
-        <nav id="nav-wrap">
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-            Show navigation
-          </a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">
-            Hide navigation
-          </a>
+                        <li>
+                            <a className="smoothscroll" href="#about">
+                                About
+                            </a>
+                        </li>
 
-          <ul id="nav" className="nav">
-            <li className="current">
-              <a className="smoothscroll" href="#home">
-                Home
-              </a>
-            </li>
-            
-            <li>
-              <a className="smoothscroll" href="#about">
-                About
-              </a>
-            </li>
+                        <li>
+                            <a className="smoothscroll" href="#resume">
+                                Resume
+                            </a>
+                        </li>
 
-            <li>
-              <a className="smoothscroll" href="#resume">
-                Resume
-              </a>
-            </li>
+                        <li>
+                            <a className="smoothscroll" href="#portfolio">
+                                Portfolio
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
 
-            <li>
-              <a className="smoothscroll" href="#portfolio">
-                Portfolio
-              </a>
-            </li>
-            
-           
-          </ul>
-        </nav>
+                <div className="row banner">
+                    <div className="banner-text">
+                        <h1 className="responsive-headline">Vinay Kumar</h1>
+                        <h3>
+                            Fourth-year student at Rutgers University currently pursuing a bachelor's degree in computer
+                            science. Developer and entrepreneur with a passion for creating innovative solutions with
+                            experience with web development, mobile development.
+                        </h3>
+                        <hr />
+                        <ul className="social">
+                            <a
+                                href="https://www.linkedin.com/in/vinay-kumar-b7527a1b4/"
+                                className="button btn project-btn"
+                            >
+                                <i className="fa fa-linkedin-square"></i>Linkedin
+                            </a>
+                            <a href="https://github.com/vinayk02" className="button btn project-btn">
+                                <i className="fa fa-github"></i>Github
+                            </a>
+                        </ul>
+                    </div>
+                </div>
 
-        <div className="row banner">
-          <div className="banner-text">
-            <h1 className="responsive-headline">Vinay Kumar</h1>
-            <h3>I am a fourth-year student at Rutgers University pursuing a bachelor's degree in computer science.</h3>
-            <hr />
-            <ul className="social">
-              <a href="https://www.linkedin.com/in/vinay-kumar-b7527a1b4/" className="button btn project-btn">
-                <i className="fa fa-linkedin-square"></i>Linkedin
-              </a>
-              <a href="https://github.com/vinayk02" className="button btn project-btn">
-                <i className="fa fa-github"></i>Github
-              </a>
-            </ul>
-          </div>
-        </div>
-
-        <p className="scrolldown">
-          <a className="smoothscroll" href="#about">
-            <i className="icon-down-circle"></i>
-          </a>
-        </p>
-      </header>
-    );
-  }
+                <p className="scrolldown">
+                    <a className="smoothscroll" href="#about">
+                        <i className="icon-down-circle"></i>
+                    </a>
+                </p>
+            </header>
+        );
+    }
 }
 
 export default Header;
